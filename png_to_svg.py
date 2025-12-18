0eee8099baaca32afe6cb3f77ff55cf03dce9f56
Clement Martin <clement.martin2.etu@univ-lille.fr>
2025-12-18 14:18:03 +0100

    feat: adding function hexagon_points

--- png_to_svg.py
+++ png_to_svg.py
@@ -1,3 +1,20 @@
 from PIL import Image
+import math
+import webbrowser
 
 image = Image.open("image/screenshot.jpg")
+image = image.convert("RGB")
+
+px = image.load()
+width, height = image.size
+
+# Retrieve all corner points of one hexagon
+def hexagon_points(cx, cy, size):
+    points = []
+
+    for i in range(6):
+        ptx = cx + size * math.cos(math.radians(60 * i - 30))
+        pty = cy + size * math.sin(math.radians(60 * i - 30))
+        points.append((ptx,pty))
+
+    return points
